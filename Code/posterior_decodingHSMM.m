cdfc52032ffa550b32226a26b105ffb7ed069b3c
Anjana Puliyanda <71105417+Anjana-T-Puliyanda@users.noreply.github.com>
2021-10-05 13:53:52 -0600

    Update posterior_decodingHSMM.m

--- Code/posterior_decodingHSMM.m
+++ Code/posterior_decodingHSMM.m
@@ -2,13 +2,13 @@ function [elnGamma]=posterior_decodingHSMM(Data,T,K,M,Mu,Cov,P,Pi,C,lambda)
 
 p=size(Data,2);
 
-elndur_p(win).window=NaN(K,T);
+elndur_p=NaN(K,T);
 for st=1:K
 	elndur_p(st,:)=eln(poisspdf(1:T,lambda(st)));
 end
   
-elnalpha(win).window=NaN(T,K); elnbeta(win).window=NaN(T,K); elnalpha_star(win).window=NaN(T,K); elnbeta_star(win).window=NaN(T,K);
-elnGamma(win).window=NaN(T,K);
+elnalpha=NaN(T,K); elnbeta=NaN(T,K); elnalpha_star=NaN(T,K); elnbeta_star=NaN(T,K);
+elnGamma=NaN(T,K);
 elnU=NaN(T,K,T);
    
 k1=(2*pi)^(-p/2);
@@ -21,15 +21,15 @@ for i=1:T
 	for l=1:K
           	ss=0;
           	for m=1:M
-			d=Mu(m).mixture(l,:)-Data_loop(win).window(i,:);
+			d=Mu(m).mixture(l,:)-Data(i,:);
 			if Cov(m).mixture(l)==0
 				Cov(m).mixture(l)=1e-40;
 			end
 			iCov=inv(Cov(m).mixture(l)); k2=k1/sqrt(det( Cov(m).mixture(l) ));
-			tempr(win).window(m).mixture(i,l)=C(l,m)*k2*exp(-0.5*d*iCov*d');
-			ss=ss+tempr(win).window(m).mixture(i,l);
+			tempr(m).mixture(i,l)=C(l,m)*k2*exp(-0.5*d*iCov*d');
+			ss=ss+tempr(m).mixture(i,l);
    		end
-          	elnB(win).window(i,l)=eln(ss);
+          	elnB(i,l)=eln(ss);
       	end
 end
     
@@ -37,27 +37,27 @@ end
 for i=1:K
 	normalizer=NaN;
         for t=1:T
-        	normalizer=elnsum(normalizer,elnB(win).window(t,i));
+        	normalizer=elnsum(normalizer,elnB(t,i));
         end
         for t=1:T
-        	elnB(win).window(t,i)=elnproduct(elnB(win).window(t,i),-normalizer);
+        	elnB(t,i)=elnproduct(elnB(t,i),-normalizer);
         end
 end
     
 
 %forward
 
-elnalpha_star(win).window(1,:)=eln(Pi);
+elnalpha_star(1,:)=eln(Pi);
 for t=1:T
 	for j=1:K
         	for d=1:T
                 	if t-d+1>0
                     		if d==1
-                    			elnU(t,j,d)=elnB(win).window(t,j);
+                    			elnU(t,j,d)=elnB(t,j);
                     		else
-                    			elnU(t,j,d)=elnproduct(elnU(t-1,j,d-1),elnB(win).window(t,j));
+                    			elnU(t,j,d)=elnproduct(elnU(t-1,j,d-1),elnB(t,j));
                     		end 
-                		elnalpha(win).window(t,j)=elnsum(elnalpha(win).window(t,j), elnproduct(elnalpha_star(win).window(t-d+1,j), elnproduct(elndur_p(win).window(j,d),elnU(t,j,d)) ) );
+                		elnalpha(t,j)=elnsum(elnalpha(t,j), elnproduct(elnalpha_star(t-d+1,j), elnproduct(elndur_p(j,d),elnU(t,j,d)) ) );
                 	end
             	end
         end
@@ -65,21 +65,21 @@ for t=1:T
     	elnScale(t)=NaN;
    	%Scaling for alpha
    	for st=1:K
-        	elnScale(win).window(t)=elnsum(elnScale(win).window(t),elnalpha(win).window(t,st));
+        	elnScale(t)=elnsum(elnScale(t),elnalpha(t,st));
    	end
    	for st=1:K
-    		elnalpha(win).window(t,st)=elnproduct(elnalpha(win).window(t,st),-elnScale(win).window(t));
+    		elnalpha(t,st)=elnproduct(elnalpha(t,st),-elnScale(t));
    	end
    
       	if t<=T-1
       		for j=1:K
           		jj_t=[];
           		for jj=1:K
-          			jj_t=[jj_t; elnproduct(elnalpha(win).window(t,jj),eln(P(jj,j)))] ;
+          			jj_t=[jj_t; elnproduct(elnalpha(t,jj),eln(P(jj,j)))] ;
           		end
-          		elnalpha_star(win).window(t+1,j)=NaN;
+          		elnalpha_star(t+1,j)=NaN;
           		for jj=1:K
-              			elnalpha_star(win).window(t+1,j)=elnsum(elnalpha_star(win).window(t+1,j),jj_t(jj));
+              			elnalpha_star(t+1,j)=elnsum(elnalpha_star(t+1,j),jj_t(jj));
           		end
           
       		end
@@ -89,7 +89,7 @@ end
 
 % Backward run
 for st=1:K
-	elnbeta(win).window(T,st)=elnproduct(1,-elnScale(win).window(T));
+	elnbeta(T,st)=elnproduct(1,-elnScale(T));
 end
 
 for t=T-1:-1:0
@@ -97,20 +97,20 @@ for t=T-1:-1:0
         	elnss=NaN;
         	for d=1:T
             		if t+d<=T
-                		elnss=elnsum( elnss, elnproduct( elndur_p(win).window(j,d), elnproduct(elnU(t+d,j,d), elnbeta(win).window(t+d,j))) );
+                		elnss=elnsum( elnss, elnproduct( elndur_p(j,d), elnproduct(elnU(t+d,j,d), elnbeta(t+d,j))) );
             		end
         	end
-        	elnbeta_star(win).window(t+1,j)=elnss;
+        	elnbeta_star(t+1,j)=elnss;
     	end
     	if t~=T-1
 		for j=1:K
 			jj_t=[];
 			for jj=1:K
-				jj_t=[jj_t; elnproduct(elnbeta_star(win).window(t+2,jj),eln(P(j,jj)))] ;
+				jj_t=[jj_t; elnproduct(elnbeta_star(t+2,jj),eln(P(j,jj)))] ;
 			end
-			elnbeta(win).window(t+1,j)=NaN;
+			elnbeta(t+1,j)=NaN;
 			for jj=1:K
-				elnbeta(win).window(t+1,j)=elnsum(elnbeta(win).window(t+1,j),jj_t(jj));
+				elnbeta(t+1,j)=elnsum(elnbeta(t+1,j),jj_t(jj));
 			end 
 
 
@@ -120,7 +120,7 @@ end
 
 for time=1:T-1
 	for st=1:K
-    		elnbeta(win).window(time,st)=elnproduct(elnbeta(win).window(time,st),-elnScale(win).window(time));  
+    		elnbeta(time,st)=elnproduct(elnbeta(time,st),-elnScale(time));  
     	end
 end
 
@@ -129,15 +129,15 @@ for time=1:T
 	normalizer=NaN;
 
     	for st=1:K
-        	elnGamma(win).window(time,st)=elnproduct(elnalpha(win).window(time,st),elnbeta(win).window(time,st));
+        	elnGamma(time,st)=elnproduct(elnalpha(time,st),elnbeta(time,st));
     	end
     
     	for st=1:K
-        	normalizer=elnsum(normalizer,elnGamma(win).window(time,st));
+        	normalizer=elnsum(normalizer,elnGamma(time,st));
     	end
     
     	for st=1:K
-        	elnGamma(win).window(time,st)=elnproduct(elnGamma(win).window(time,st),-normalizer);
+        	elnGamma(time,st)=elnproduct(elnGamma(time,st),-normalizer);
     	end
 
         
